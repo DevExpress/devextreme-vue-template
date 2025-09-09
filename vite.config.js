52e21547258ee70c8acb10d5dabd0d353fb489c3
Arman Jivanyan <arman.jivanyan@devexpress.com>
2025-09-09 17:31:12 +0400

    fix base url

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/devextreme-vue-template/',
   plugins: [
     vue(),
     vueDevTools(),
