1c70a60d9f13d08915ae5f40f2d97c14a4c5ab21
Arman Jivanyan <arman.jivanyan@devexpress.com>
2025-09-10 15:27:19 +0400

    fix path issue

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/devextreme-vue-template/',
   plugins: [
     vue(),
     vueDevTools(),
