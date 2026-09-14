b5f1396f8a3ea8b5f2c43f94617bc72533cd9684
Andrei Vorobev <738482+vorobey@users.noreply.github.com>
2026-09-14 13:31:00 +0300

    build: add sanitze for names

--- vite.config.js
+++ vite.config.js
@@ -15,4 +15,15 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        sanitizeFileName: (name) => {
+          return name
+            .replace(/\s+/g, '-')
+            .replace(/[^a-zA-Z0-9_.-]/g, '');
+        },
+      },
+    },
+  },
 })
